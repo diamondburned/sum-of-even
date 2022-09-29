51acfcda0fe4ceb12365897e05d8c9f2646c6927
diamondburned <diamond@arikawa-hi.me>
2022-09-28 23:29:50 -0700

    Clean up reflect.SliceHeader

--- sum-branchless_amd64.go
+++ sum-branchless_amd64.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"reflect"
 	"runtime"
 	"unsafe"
 )
 
 //go:noescape
-func _sum_branchless(arrayPtr uintptr, arrayLen int64, sum *int64)
+func _sum_branchless(arrayPtr unsafe.Pointer, arrayLen int64, sum *int64)
 
 func sumOfEvenBranchlessSIMD(vs []int) int {
 	var sum int64
-	header := (*reflect.SliceHeader)(unsafe.Pointer(&vs))
-	_sum_branchless(header.Data, int64(header.Len), &sum)
+	_sum_branchless(unsafe.Pointer(&vs[0]), int64(len(vs)), &sum)
 	runtime.KeepAlive(vs)
 	return int(sum)
 }
--- sum_amd64.go
+++ sum_amd64.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"reflect"
 	"runtime"
 	"unsafe"
 )
 
 //go:noescape
-func _sum(arrayPtr uintptr, arrayLen int64, sum *int64)
+func _sum(arrayPtr unsafe.Pointer, arrayLen int64, sum *int64)
 
 func sumOfEvenSIMD(vs []int) int {
 	var sum int64
-	header := (*reflect.SliceHeader)(unsafe.Pointer(&vs))
-	_sum(header.Data, int64(header.Len), &sum)
+	_sum(unsafe.Pointer(&vs[0]), int64(len(vs)), &sum)
 	runtime.KeepAlive(vs)
 	return int(sum)
 }
